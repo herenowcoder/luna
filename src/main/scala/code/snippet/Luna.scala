ce10c979eae0e6ad60c6ff1c88318b9369ee5330
Wojtek Kaczmarek <wojtek@yexperiment.com>
2015-10-12 20:15:51 +0200

    simple moonpixSwitch

--- src/main/scala/code/snippet/Luna.scala
+++ src/main/scala/code/snippet/Luna.scala
@@ -3,20 +3,21 @@ package code.snippet
 import scala.xml.NodeSeq
 import net.liftweb.util.Helpers._
 import net.liftweb.http.js.{JsExp,JE}
-import net.liftweb.http.js.JsCmds.{SetHtml} // needed very soon
+import net.liftweb.http.js.jquery.JqJE.{Jq,JqAttr}
 
 class Luna {
   val moonpixPrefix = "/imported/luna-ngen/images/"
-  val moonpixList = Seq("Moonburn_small.jpg", "Golden_Moon_small.jpg")
+  val moonpixList = Array("Moonburn_small.jpg", "Golden_Moon_small.jpg")
 
   def moonpix = {
-    "a [onclick]" #> moonpixFade(1 second, 2 seconds) &
+    "a [onclick]" #> moonpixSwitch(moonpixList(1), 1 second, 2 seconds) &
     "img [src]"   #> (moonpixPrefix + moonpixList.head)
   }
 
-  private def moonpixFade(fadeOutTime: TimeSpan, fadeInTime: TimeSpan): JsExp = {
+  private def moonpixSwitch(newMoonpix: String, fadeOutTime: TimeSpan, fadeInTime: TimeSpan): JsExp = {
     val fade = "$(this).fadeTo"
     JE.Call(fade, fadeOutTime.toMillis, 0.01, JE.AnonFunc(
+      JE.Call("$('img', this).attr", "src", moonpixPrefix+newMoonpix).cmd &
       JE.Call(fade, fadeInTime.toMillis, 1.0).cmd
     ))
   }
