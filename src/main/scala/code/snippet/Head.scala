4fdf7c61adc7caae0173562adcc1df9f4d721a64
Wojtek Kaczmarek <wojtekk@kofeina.net>
2015-10-13 06:28:28 +0200

    types in Head snippet

--- src/main/scala/code/snippet/Head.scala
+++ src/main/scala/code/snippet/Head.scala
@@ -1,7 +1,8 @@
 package code.snippet
 
+import net.liftweb.util.CssSel
 import net.liftweb.util.Helpers._
 
 class MyHead {
-    def title = "title *" #> "luna.inthephase lifted"
+    def title: CssSel = "title *" #> "luna.inthephase lifted"
 }
