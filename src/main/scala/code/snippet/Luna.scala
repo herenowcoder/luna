9ec47a4a44ba68ace374cd00d7917171d6f6a02f
Wojtek Kaczmarek <wojtekk@kofeina.net>
2015-10-15 01:50:31 +0200

    snippet impl returns "constants" where possible

--- src/main/scala/code/snippet/Luna.scala
+++ src/main/scala/code/snippet/Luna.scala
@@ -10,7 +10,9 @@ class Luna {
   val moonpixPrefix = "/imported/luna-ngen/images/"
   val moonpixList = Array("Moonburn_small.jpg", "Golden_Moon_small.jpg")
 
-  def moonpix: CssSel = {
+  // snippet impl changed to val as (for now) this needs to get evaluated
+  // only once
+  val moonpix: CssSel = {
     "a [onclick]" #> moonpixSwitch(moonpixList(1), 1 second, 2 seconds) &
     "img [src]"   #> (moonpixPrefix + moonpixList.head)
   }
@@ -26,7 +28,7 @@ class Luna {
     ))
   }
 
-  def logo: NodeSeq = (
+  val logo: NodeSeq = (
     <p/>
     <span style="font-size:56px; font-family:Tahoma,Arial,sans-serif">
         luna.inthephase
@@ -34,7 +36,7 @@ class Luna {
     <p/>
   )
 
-  def placeholder: NodeSeq = (
+  val placeholder: NodeSeq = (
     <p/>
     <div style=
       "font:italic small-caps 72px fantasy; text-align:center; color:#fec">
@@ -42,7 +44,7 @@ class Luna {
     </div>
   )
 
-  def copyright: NodeSeq = (
+  val copyright: NodeSeq = (
     <div id="copyright"
       style="font:small-caps 15px normal; text-align:center; color:#ccc">
       © <span style="font-size:80%">2015 </span>
