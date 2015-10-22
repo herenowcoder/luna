c097fff2d4daecd69e75f2980216c0a46dd72785
Wojtek Kaczmarek <wojtekk@kofeina.net>
2015-10-23 00:45:03 +0200

    MyHead.title: val instead of def

--- src/main/scala/code/snippet/Head.scala
+++ src/main/scala/code/snippet/Head.scala
@@ -4,5 +4,5 @@ import net.liftweb.util.CssSel
 import net.liftweb.util.Helpers._
 
 class MyHead {
-    def title: CssSel = "title *" #> "luna.inthephase lifted"
+    val title: CssSel = "title *" #> "luna.inthephase lifted"
 }
