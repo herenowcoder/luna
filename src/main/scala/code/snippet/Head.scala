1a95f84268723745a1e88c7a6595352f5c3ee4ea
Wojtek Kaczmarek <wojtekk@kofeina.net>
2015-10-23 01:07:21 +0200

    MyHead: one snippet call for all metadata

--- src/main/scala/code/snippet/Head.scala
+++ src/main/scala/code/snippet/Head.scala
@@ -4,5 +4,12 @@ import net.liftweb.util.CssSel
 import net.liftweb.util.Helpers._
 
 class MyHead {
-    val title: CssSel = "title *" #> "luna.inthephase lifted"
+  val metadata: CssSel = {
+    "title *" #> "luna.inthephase lifted" &
+    "@author      [content]" #> "Dual Tech" &
+    "@description [content]" #> "All things lunar." &
+    "@keywords    [content]" #>
+      """moon, moon phase, luna, lunar, luna in the phase,
+      moon in the phase, in the phase"""
+  }
 }
